c9f3a0d57b6823f40616eef9de8fe09ddfc89bc0
Ritik <gritik418.dev@gmail.com>
2024-04-17 23:58:39 +0530

    Email verification route completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,6 +8,7 @@ import vine, { errors } from "@vinejs/vine";
 import loginSchema from "../validators/loginSchema.js";
 import { ErrorReporter } from "../validators/ErrorReporter.js";
 import signupSchema from "../validators/signupSchema.js";
+import verificationSchema from "../validators/verificationSchema.js";
 
 vine.errorReporter = () => new ErrorReporter();
 
@@ -59,7 +60,11 @@ export const userLogin = async (req, res) => {
     });
   } catch (error) {
     if (error instanceof errors.E_VALIDATION_ERROR) {
-      console.log(error.messages);
+      return res.status(400).json({
+        success: false,
+        status: 400,
+        errors: error.messages,
+      });
     }
     return res.status(500).json({
       success: false,
@@ -133,7 +138,81 @@ export const userSignUp = async (req, res) => {
     });
   } catch (error) {
     if (error instanceof errors.E_VALIDATION_ERROR) {
-      console.log(error.messages);
+      return res.status(400).json({
+        success: false,
+        status: 400,
+        errors: error.messages,
+      });
+    }
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+      status: 400,
+    });
+  }
+};
+
+export const verifyEmail = async (req, res) => {
+  try {
+    const data = req.body;
+    const output = await vine.validate({
+      schema: verificationSchema,
+      data,
+    });
+
+    const user = await User.findOne({ email: output.email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        status: 400,
+        message: "Invalid Request.",
+      });
+    }
+
+    const verificationToken = await EmailVerification.findOne({
+      userId: user._id,
+    });
+
+    if (!verificationToken) {
+      return res.status(401).json({
+        success: false,
+        status: 400,
+        message: "OTP Expired.",
+      });
+    }
+
+    const verifyToken = await bcrypt.compare(
+      output.otp,
+      verificationToken.secretKey
+    );
+
+    if (!verifyToken) {
+      return res.status(401).json({
+        success: false,
+        status: 400,
+        message: "Invalid OTP.",
+      });
+    }
+
+    const token = await user.generateAuthToken({
+      id: user._id,
+      email: user.email,
+    });
+
+    return res.status(201).json({
+      success: true,
+      status: 200,
+      token: token,
+      message: "Account created Successfully..",
+    });
+  } catch (error) {
+    if (error instanceof errors.E_VALIDATION_ERROR) {
+      return res.status(400).json({
+        success: false,
+        status: 400,
+        errors: error.messages,
+      });
     }
     return res.status(500).json({
       success: false,
